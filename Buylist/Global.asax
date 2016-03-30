b05f5db6cd938b2182d75bb8d6dc30a5d5243b01
igortavares82 <igor.meier@gmail.com>
2016-03-30 17:05:24 -0300

    Criação do projeto

---
+++ Buylist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Buylist.MvcApplication" Language="C#" %>
---
+++ Buylist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Buylist.Startup))]
+namespace Buylist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
