bf40b9da998fd8ffac437efb65affc7594649c91
igortavares82 <igor.meier@gmail.com>
2016-03-30 21:38:23 -0300

    Criação do projeto de testes

---
+++ Buylist.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Buylist.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
